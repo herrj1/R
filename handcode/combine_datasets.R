09f653f20a0146b88b2e1ac74f16299ca1e585f6
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-09-17 23:10:24 -0400

    Update combine_datasets.R

--- handcode/combine_datasets.R
+++ handcode/combine_datasets.R
@@ -1,21 +1,22 @@
 stuff1<-read.table("stuff1.txt",header=TRUE,sep=",")
 stuff1.label<-rep(1,100)
-stuff1<-cbind(stuff1,stuff1.label)
-names(stuff1)<-c("weight","height","label")
+stuff1<-cbind(stuff1,stuff1.label) #combine label column 
+names(stuff1)<-c("weight","height","label") #assign column labels to top row 
 
 stuff2<-read.table("stuff2.txt",header=TRUE,sep=",")
 stuff2.label<-rep(-1,100)
-stuff2<-cbind(stuff2,stuff2.label)
-names(stuff2)<-c("weight","height","label")
+stuff2<-cbind(stuff2,stuff2.label) #combine label column
+names(stuff2)<-c("weight","height","label") #assign column labels to top row 
+
 
 stuff1.2<-rbind(stuff1,stuff2)
 d.set<-data.frame(cbind(rep(1,200),stuff1.2))
 names(d.set)<-c("bias","weight","height","label")
 d.set
 
-train.index<-sample(nrow(d.set),nrow(d.set)*0.3)
-train.index
+train.index<-sample(nrow(d.set),nrow(d.set)*0.3) #get 30% of data
+train.index #Shows data
 
 training.set<-d.set[train.index,]
 test.set<-d.set[-train.index,]
-training.set //shows training dataset 
+training.set #shows training dataset 
