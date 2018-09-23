e40f76083ea5e45d92183db5d827e38adf7d59a4
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-09-23 13:38:42 -0400

    Update app4.r

--- analytics/app4/app4.r
+++ analytics/app4/app4.r
@@ -37,67 +37,40 @@ head(y)
 perceptron <-function(x,y,eta,niter){
 
 	#initializes weight vector
-
 	weight<-rep(0,dim(x)[2]+1)
-	
-errors<-rep(0,niter)
+	errors<-rep(0,niter)
 
 	# Predict binary label using Heaviside activation 
         # function
         z <- sum(weight[2:length(weight)] * 
                          as.numeric(x[ii, ])) + weight[1]
-
 	
-#loop over number of epochs niter
-	
+	#loop over number of epochs niter
+	for(jj in 1:niter){
+		for(ii in 1:length(y)){
+			if(z<0){
+				ypred<- -1
+			}else{
+				ypred <- 1
+			}
 
-for(jj in 1:niter){
-		
-
-for(ii in 1:length(y)){
-			
-if(z<0){
-				
-ypred<- -1
-			
-}else{
-				
-ypred <- 1
-			
-}
-	
-#change weight - the formula doesn't do
-	
-#anything if the predicted value is correct
-
+		#change weight - the formula doesn't do
+		#anything if the predicted value is correct
 		weightdiff <- eta * (y[ii] - ypred)* c(1, as.numeric(x[ii,]))
-
 		weight<-weight+ weightdiff
-
 		
-if((y[ii] - ypred) != 0.0){
-
+		if((y[ii] - ypred) != 0.0){
 			errors[jj] <- errors[jj] + 1
-}
-
 		}
-	
+       	}
+   }
+   #weight to decide between the two species
+   print(weight)
+   return(errors)
 }
-#weight to decide between the two species
-	
-print(weight)
-
-	return(errors)
-
-}
-
 err<-perceptron(x,y,1,10)
 
-
-
 plot(1:10, err, type="l", lwd=2, col="red", xlab="epoch #", ylab="errors")
-
 title("Errors vs epoch - learning rate eta = 1")
-
 
 savehistory("~/Projects/R/analytics/app4/app4.r")
