a20ea44db43a0d19075b01d55ec6a55c6da70b8a
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-06-29 23:55:54 -0400

    Update index.r

--- webdata/app1/index.r
+++ webdata/app1/index.r
@@ -1,6 +1,6 @@
 #Installs the data table package
 install.packages("data.table")
-#import libraries into working project
+#import libraries into working projects
 library(data.table)
 mydata <- fread('http://www.stats.ox.ac.uk/pub/datasets/csb/97532.dat')
 head(mydata)
